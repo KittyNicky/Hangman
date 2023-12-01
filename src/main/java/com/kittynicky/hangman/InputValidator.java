3ab7cabd0e9598529d39d1ab894dbee2d3426bbb
KittyNicky <zolnikov.nik@gmail.com>
2023-12-01 23:00:03 +0600

    refactoring

--- src/main/java/com/kittynicky/hangman/InputValidator.java
+++ src/main/java/com/kittynicky/hangman/InputValidator.java
@@ -2,10 +2,18 @@ package com.kittynicky.hangman;
 
 public class InputValidator {
     public static boolean validate(Character c) {
-        return c >= 'а' && c <= 'я';
+        if (c < 'а' || c > 'я') {
+            System.out.println("Неизвестный символ. Попробуйте снова.\n");
+            return false;
+        }
+        return true;
     }
 
     public static boolean validate(String s) {
-        return s.length() == 1;
+        if (s.length() != 1) {
+            System.out.println("Введено более 1 символа. Попробуйте снова.\n");
+            return false;
+        }
+        return true;
     }
 }
